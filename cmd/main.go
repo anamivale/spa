50ff346f8878acbfaa10cca0d1358f7a5ead3dae
Valeria Muhembele <muhembelev@gmail.com>
2025-04-07 08:47:56 +0300

    initialize modules in project

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"spa/db"
+	"spa/handlers"
+)
+
+func main() {
+	db.Init()
+	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web"))))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { http.ServeFile(w, r, "web/index.html") })
+	http.HandleFunc("/register", handlers.Register)
+	fmt.Println("http://localhost:8080")
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module spa
+
+go 1.24.1
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/mattn/go-sqlite3 v1.14.27
+	golang.org/x/crypto v0.37.0
+)
