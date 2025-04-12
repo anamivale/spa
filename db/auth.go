22bfe1faf7bb86f66ec5bdf84d949c58f0451815
vmuhembe <muhembelev@gmail.com>
2025-04-12 17:27:16 +0300

    feat: add functions to check if user exist and add new user

--- db/auth.go
+++ db/auth.go
@@ -1,10 +1,21 @@
 package db
 
 import (
+	"database/sql"
+	"errors"
 	"spa/models"
+	"spa/utils"
 )
 
 func InsertIntoUsersTable(id string, resData models.Signup) error {
+	exists, err1 := CheckIfUserAlredyExist(resData.Nickname,resData.Email)
+	if err1 != nil {
+		return errors.New("problem registering, try again")
+	}
+	if exists {
+		return errors.New("email or nickname already taken")
+	}
+
 	query := `
 	INSERT INTO users (ID,nickname,age, gender, fname, lname, email, password) VALUES (?,?,?,?,?,?,?,?)
 	`
@@ -14,12 +25,30 @@ func InsertIntoUsersTable(id string, resData models.Signup) error {
 	if err != nil {
 		return err
 	}
+	password, err := utils.EncryptPassword(resData.Password)
+	if err != nil {
+		return errors.New("problem registering, try again")
+	}
 
-	_, err = Db.Exec(query, id, resData.Nickname, resData.Age, resData.Gender, resData.Fname, resData.Lname, resData.Email, resData.Password)
+	_, err = Db.Exec(query, id, resData.Nickname, resData.Age, resData.Gender, resData.Fname, resData.Lname, resData.Email, password)
 
 	if err != nil {
 
-		return err
+		return errors.New("problem registering, try again")
 	}
 	return nil
+}
+
+func CheckIfUserAlredyExist(nickname, email string) (bool, error) {
+
+	var exists bool
+
+	err = Db.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE nickname=? Or email=?)", nickname, email).Scan(&exists)
+
+	if err != nil && err != sql.ErrNoRows {
+		return false, err
+	}
+
+	return exists, nil
+
 }
