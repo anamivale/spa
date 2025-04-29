35eb14a03734d17288012da00795c1bbb4665b44
vmuhembe <muhembelev@gmail.com>
2025-04-30 01:39:17 +0300

    feat: get img url from the db

--- db/feeds.go
+++ db/feeds.go
@@ -4,7 +4,7 @@ import "spa/models"
 
 func GetPosts() ([]models.PostResponse, error) {
 
-	query := "SELECT title, content, categories, created_at FROM posts"
+	query := "SELECT title, content, categories, created_at, imgurl FROM posts"
 	var output []models.PostResponse
 
 	 rows,err := Db.Query(query)
@@ -14,7 +14,7 @@ func GetPosts() ([]models.PostResponse, error) {
 
 	 for rows.Next(){
 		var post models.PostResponse
-		err := rows.Scan(&post.Title, &post.Content, &post.Categories, &post.CreatedAt)
+		err := rows.Scan(&post.Title, &post.Content, &post.Categories, &post.CreatedAt, &post.Imgurl)
 		if err != nil {
 			return nil, err
 		 }
