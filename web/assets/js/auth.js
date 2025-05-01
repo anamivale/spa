a6231e7efeb7fd8ef188aa306f80822016a5598a
sjuma <sheillajuma001@gmail.com>
2025-05-01 18:07:41 +0300

    defining login function in auth.js

--- web/assets/js/auth.js
+++ web/assets/js/auth.js
@@ -87,6 +87,27 @@ export function Signup() {
 
   }
 }
+export function Login() {
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+
+  fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error("Login failed");
+      return res.json();
+    })
+    .then((data) => {
+      console.log("Login successful:", data);
+      location.reload();
+    })
+    .catch((err) => alert(err.message));
+}
+
+
 
 export function loadAuthView(error) {
   document.getElementById("app").innerHTML = authTemplate();
