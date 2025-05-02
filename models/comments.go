38149c490505e7beeb5d8211e8e50c46c560a302
sjuma <sheillajuma001@gmail.com>
2025-05-02 17:01:09 +0300

    fix(models): change comment model IDs from int to string to support UUIDs

--- models/comments.go
+++ models/comments.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Comment struct {
-	CommentID    int       `db:"comment_id"`
-	PostID       int       `db:"post_id"`
-	UserID       int       `db:"user_id"`
+	CommentID    string    `db:"comment_id"`
+	PostID       string    `db:"post_id"`
+	UserID       string    `db:"user_id"`
 	Content      string    `db:"content"`
 	Username     string    `db:"user_name"`
 	CreatedAt    string    `db:"created_at"`
