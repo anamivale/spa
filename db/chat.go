445045ca46ae0d7c099358e03b801315c30bffb9
vmuhembe <muhembelev@gmail.com>
2025-05-07 17:27:45 +0300

    feat: add messAGES table to db

---
+++ db/chat.go
@@ -0,0 +1,24 @@
+package db
+
+import "log"
+
+func MessageTable() {
+	createTables := `
+	
+	
+	CREATE TABLE IF NOT EXISTS messages (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		sender_id TEXT NOT NULL,
+		receiver_id TEXT NOT NULL,
+		content TEXT NOT NULL,
+		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+		read BOOLEAN DEFAULT 0,
+		FOREIGN KEY (sender_id) REFERENCES users (user_id),
+		FOREIGN KEY (receiver_id) REFERENCES users (user_id)
+	);
+	`
+	_, err = Db.Exec(createTables)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
