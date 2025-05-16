889b943b73f2396ba8d41555ca3595401e9b5a10
a-j-sheilla <sheillajuma001@gmail.com>
2025-05-16 13:51:52 +0300

    fix: function call to match the correct function name ...CreateSessionTable() and CreateSessionsTable()

--- db/init.go
+++ db/init.go
@@ -3,7 +3,6 @@ package db
 import (
 	"database/sql"
 	"fmt"
-
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -11,16 +10,15 @@ var Db *sql.DB
 var err error
 
 func Init() {
-
 	Db, err = sql.Open("sqlite3", "forum.db")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	
 	createUserTable()
 	CreateSessionTable()
 	CreatePostTable()
 	CommentsTable()
 	ReactionTable()
 	MessageTable()
-
 }
