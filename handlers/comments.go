3707aa3938992e9640b610c25230f1beb9fddda4
a-j-sheilla <sheillajuma001@gmail.com>
2025-05-16 14:05:20 +0300

    Refactor HandleCreateComment: Add error handling for GetUser and fix user data mapping

--- handlers/comments.go
+++ handlers/comments.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"spa/db"
 	"spa/models"
@@ -22,9 +23,18 @@ func HandleCreateComment(w http.ResponseWriter, r *http.Request) {
 	}
 
 	comment.CommentID = utils.GenerateUUid()
-	user := db.GetUser(r)
-	comment.UserID = user[2]
-	comment.Username = user[0]
+	user, err := db.GetUser(r)
+	if err != nil {
+		fmt.Println(err.Error())
+		response := map[string]interface{}{
+			"type": err.Error(),
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	comment.UserID = user.Id
+	comment.Username = user.Nickname
 
 	err = db.InsertComment(comment)
 	if err != nil {
