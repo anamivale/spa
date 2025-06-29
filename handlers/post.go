09dd71c721659f9d561b20cd6df376ea7e40f261
anamivale <muhembelev@gmail.com>
2025-06-29 15:37:07 +0300

    fix: correct typo in error message and add validation for post fields

--- handlers/post.go
+++ handlers/post.go
@@ -7,6 +7,7 @@ import (
 	"spa/db"
 	"spa/models"
 	"spa/utils"
+	"strings"
 )
 
 func HandleCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +15,7 @@ func HandleCreatePost(w http.ResponseWriter, r *http.Request) {
 	var data models.Post
 	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
-		fmt.Println("faild to read data. Error: ", err.Error())
+		fmt.Println("failed to read data. Error: ", err.Error())
 		response := map[string]interface{}{
 			"type": err.Error(),
 		}
@@ -23,6 +24,23 @@ func HandleCreatePost(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(response)
 		return
 	}
+
+	// Validate that at least one field (title, content, or image) is provided
+	hasTitle := strings.TrimSpace(data.Title) != ""
+	hasContent := strings.TrimSpace(data.Content) != ""
+	hasImage := data.Imgurl != "" || data.ImgContent != ""
+
+	if !hasTitle && !hasContent && !hasImage {
+		response := map[string]interface{}{
+			"type":    "error",
+			"message": "Please provide at least one of the following: title, content, or image",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
 	data.PostID = utils.GenerateUUid()
 
 	if data.Imgurl != "" {
